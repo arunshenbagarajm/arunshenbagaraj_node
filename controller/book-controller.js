e7444eb0a8ed83fbf56aa56d512d568060825eb7
Arun Shenbagaraj MAnoharan <arun.shenbagaraj@zemosolabs.com>
2022-11-09 11:21:57 +0530

    filter methods added

--- controller/book-controller.js
+++ controller/book-controller.js
@@ -1,4 +1,6 @@
 const Book = require('../models/book');
+const sequelize = require('../util/databse');
+const Op = sequelize.Sequelize.Op;
 
 exports.getAllBooks = (req, res, next) => {
     Book.findAll().then(result => {
@@ -39,6 +41,45 @@ exports.updateBook = (req, res, next) => {
     Book.update(req.body, {
         where : {id: id}
     }).then(result => {
+        res.send(result);
+    }).catch(error => {
+        console.log(error);
+        res.send("Error occured");
+    });
+};
+
+exports.deleteBook = (req, res, next) => {
+    const id = req.params.id;
+
+    Book.destroy({ where: {id: id}}).then(result => {
+        if (result === 1) {
+            res.send("Deleted successfully");
+        } else {
+            res.status(404).send({
+                message: `Cannot find Book with id=${id}.`
+            });
+        }
+    }).catch(error => {
+        console.log(error);
+        res.send("Error occured");
+    });
+};
+
+exports.findByTitle = (req, res, next) => {
+    const title = req.params.title;
+    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+    Book.findAll({ where: condition }).then(result => {
+        res.send(result);
+    }).catch(error => {
+        console.log(error);
+        res.send("Error occured");
+    });
+};
+
+exports.findByAuthor = (req, res, next) => {
+    const author = req.params.author;
+    var condition = author ? { author: { [Op.like]: `%${author}%` } } : null;
+    Book.findAll({ where: condition }).then(result => {
         res.send(result);
     }).catch(error => {
         console.log(error);
--- routes/book-router.js
+++ routes/book-router.js
@@ -8,5 +8,8 @@ router.get('/', bookController.getAllBooks);
 router.get('/:id', bookController.getBook);
 router.post('/', bookController.createBook);
 router.put('/:id', bookController.updateBook);
+router.delete('/:id', bookController.deleteBook);
+router.get('/title/:title', bookController.findByTitle);
+router.get('/author/:author', bookController.findByAuthor);
 
 module.exports = router;
